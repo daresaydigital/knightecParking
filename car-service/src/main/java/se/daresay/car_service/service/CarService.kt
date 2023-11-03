84e6c03df9c666e7db202be7cd51d396f43ba66a
Ali Shobeyri <ali.shobeyri@daresay.co>
2023-11-03 15:40:04 +0100

    remove splash screen move the logic into CarService instead

--- car-service/src/main/java/se/daresay/car_service/service/CarService.kt
+++ car-service/src/main/java/se/daresay/car_service/service/CarService.kt
@@ -5,8 +5,8 @@ import androidx.car.app.Session
 import androidx.car.app.validation.HostValidator
 import se.daresay.car_service.db.TOKEN
 import se.daresay.car_service.db.load
-import se.daresay.car_service.screen.login.SignInUserNameScreen
-import se.daresay.car_service.screen.parkings.ParkingAreaListScreen
+import se.daresay.car_service.screen.login.SignInScreen
+import se.daresay.car_service.screen.parkings.ParkingOfficeListScreen
 import se.daresay.car_service.session.MainSession
 
 class CarService: CarAppService() {
@@ -15,14 +15,12 @@ class CarService: CarAppService() {
 
     override fun onCreateSession(): Session {
         val token = load(TOKEN)
-        if (token == null){
-            MainSession()
-            screenManager.push(SignInUserNameScreen(carContext))
-            screenManager.remove(this@SplashScreen)
+        val clazz = if (token == null){
+            SignInScreen::class
         } else {
-            screenManager.push(ParkingAreaListScreen(carContext))
-            screenManager.remove(this@SplashScreen)
+            ParkingOfficeListScreen::class
         }
+        return MainSession(clazz)
     }
 
     override fun onCreate() {
--- car-service/src/main/java/se/daresay/car_service/session/MainSession.kt
+++ car-service/src/main/java/se/daresay/car_service/session/MainSession.kt
@@ -3,11 +3,16 @@ package se.daresay.car_service.session
 import android.content.Intent
 import androidx.car.app.Screen
 import androidx.car.app.Session
-import se.daresay.car_service.screen.SplashScreen
-import se.daresay.car_service.screen.login.SignInUserNameScreen
+import se.daresay.car_service.screen.BaseScreen
+import se.daresay.car_service.screen.login.SignInScreen
+import se.daresay.car_service.screen.parkings.ParkingOfficeListScreen
 import kotlin.reflect.KClass
 
-class MainSession(val screenType: KClass<Screen>) : Session() {
+class MainSession(val screenType: KClass<out BaseScreen>) : Session() {
     override fun onCreateScreen(intent: Intent): Screen {
+        if (screenType == SignInScreen::class)
+            return SignInScreen(carContext)
+        else
+            return ParkingOfficeListScreen(carContext)
     }
 }
