591caaeaf681ef16b275b1c7a107bad51c9dbf27
Yauheni <yauheni.slizh@knightec.se>
2023-11-08 15:58:30 +0100

    Fixed Koin initialization for Android Automotive

--- car-service/src/main/java/se/daresay/car_service/service/CarService.kt
+++ car-service/src/main/java/se/daresay/car_service/service/CarService.kt
@@ -3,16 +3,38 @@ package se.daresay.car_service.service
 import androidx.car.app.CarAppService
 import androidx.car.app.Session
 import androidx.car.app.validation.HostValidator
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext
+import org.koin.core.context.stopKoin
 import org.koin.java.KoinJavaComponent.get
 import se.daresay.car_service.db.Editor
 import se.daresay.car_service.db.TOKEN
+import se.daresay.car_service.di.editorModule
+import se.daresay.car_service.di.viewModelModule
 import se.daresay.car_service.screen.login.SignInScreen
 import se.daresay.car_service.screen.parkings.ParkingOfficeListScreen
 import se.daresay.car_service.session.MainSession
+import se.daresay.data.di.apiModule
+import se.daresay.data.di.repoModule
+import se.daresay.data.di.usecaseModule
 
 class CarService: CarAppService() {
 
-    val editor: Editor = get(Editor::class.java)
+    private lateinit var editor: Editor
+
+    init {
+        stopKoin()
+        GlobalContext.startKoin {
+            // Log Koin into Android logger
+            androidLogger()
+            // Reference Android context
+            androidContext(this@CarService)
+            // Load modules
+            modules(editorModule, apiModule, repoModule, viewModelModule, usecaseModule)
+        }
+    }
+
     override fun createHostValidator(): HostValidator =
         HostValidator.ALLOW_ALL_HOSTS_VALIDATOR
 
@@ -28,5 +50,6 @@ class CarService: CarAppService() {
 
     override fun onCreate() {
         super.onCreate()
+        editor = get(Editor::class.java)
     }
 }
